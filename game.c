28b8632524f543d7741a200dd9f400e83cea3e1c
Nolan <nolanb414@yahoo.com>
2022-09-04 16:13:23 -0400

    Added main program
    I added the main program and wrote out the includes and defenitions

---
+++ game.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <curses.h>
+#include <time.h>
+
+#ifndef FPS
+
+#define FPS 10
+
+#endif
+
+
+int main(int argc, char** argv) {
+	return EXIT_SUCCESS;
+}
+
